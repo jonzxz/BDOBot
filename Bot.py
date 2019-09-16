86d54bddba54b7569d22c2f77a1645c734b89478
Jon <jonathankerk@gmail.com>
2019-09-16 23:59:27 +0800

    boss spawn reminder feature added, welcome message fixed

--- Bot.py
+++ Bot.py
@@ -1,14 +1,17 @@
 from Scraper import Scraper
 import discord
 from discord.ext import commands
-from utils import nextboss, mpcheck, get_token
+from utils import nextboss, mpcheck, get_token, timediff
+from discord.utils import get, find
+import asyncio
+
+
+
 
 def start():
     nezuko = commands.Bot(command_prefix='%')
     scraper = Scraper()
     nezuko.remove_command('help')
-
-
 
     @nezuko.event
     async def on_ready():
@@ -16,13 +19,47 @@ def start():
         print(nezuko.user.name)
         print(nezuko.user.id)
 
-        await nezuko.change_presence(activity=discord.Activity(name='Pastries, use %help', type=3))
+    @nezuko.event
+    async def on_member_join(member):
+        chn = nezuko.get_channel(574641679575941137)
+        await chn.send('Welcome ' + member.mention + ' to Pastries!\n\n Head over to #rules and change your discord nickname to your family name.\nEnjoy your stay!')
 
-    #Not working
     @nezuko.command()
-    async def on_member_join(chn, member):
-        print("eee")
-        await chn.send('Welcome ' + member + ' to Pastries!')
+    async def bosshunter(message):
+        role = get(message.guild.roles, name='Boss Hunter')
+        member = message.author
+        chn = message.channel
+        bh = find(lambda r: r.name == 'Boss Hunter', member.roles)
+        if bh is None:
+            await member.add_roles(role)
+            await chn.send("{0.author.mention} is now registered as a boss hunter!".format(message))
+        else:
+            await member.remove_roles(role)
+            await chn.send("{0.author.mention} have resigned as a boss hunter!".format(message))
+
+
+    @nezuko.event
+    async def change_presence():
+        await nezuko.wait_until_ready()
+        while True:
+            await nezuko.change_presence(activity=discord.Activity(name='Pastries', type=3))
+            await asyncio.sleep(10)
+            await nezuko.change_presence(activity=discord.Activity(name='use %help', type=1))
+            await asyncio.sleep(10)
+
+
+    @nezuko.event
+    async def boss_reminder():
+        await nezuko.wait_until_ready()
+        # pastries boss hunter
+        chn = nezuko.get_channel(574646616489721887)
+        for role in nezuko.get_guild(574641536214630401).roles:
+            if role.name == 'Boss Hunter':
+                bh = role
+        while not nezuko.is_closed():
+            if timediff(nextboss().get_time()) == 1800:
+                await chn.send(bh.mention + ', ' + nextboss().get_name() + ' will spawn in 30 minutes time!')
+            await asyncio.sleep(1)
 
     @nezuko.command()
     async def help(chn):
@@ -32,6 +69,7 @@ def start():
                 "%help - shows this message\n" \
                 "%foodname - retrieves ingredients i.e. %5 beer\n" \
                 "%wb - shows the upcoming world boss\n" \
+                "%bosshunter - (un)register the Boss Hunter role. Receive notifications 30 minutes before world boss!\n" \
                 "%mp - shows profit with(out) VP from selling in MP i.e. %mp 43500000\n" \
                 "%bug <message>- reports a bug to Kagi\n" \
                 "```"
@@ -39,7 +77,9 @@ def start():
 
     @nezuko.command()
     async def wb(chn):
-         await chn.send('```' + nextboss() + '```')
+        msg = ('The next boss is ' + nextboss().get_name() + ' at ' + nextboss().get_time().strftime('%H:%M') + 'hrs, GMT+8')
+        await chn.send('```' + msg + '```')
+
 
     @nezuko.command()
     async def mp(chn, silver):
@@ -76,4 +116,6 @@ def start():
             return
         raise error
 
+    nezuko.loop.create_task(boss_reminder())
+    nezuko.loop.create_task(change_presence())
     nezuko.run(get_token())
