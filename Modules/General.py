59d331931bc74edeec0b211bff4e35e3883df62b
Jon <jonathankerk@gmail.com>
2019-09-28 10:07:39 +0800

    updated welcome message

--- Modules/General.py
+++ Modules/General.py
@@ -23,14 +23,15 @@ class General(commands.Cog):
         ctx = member.guild.system_channel
         if ctx is not None:
             await ctx.send('Hello fresh dough {0.mention}, welcome to Pastries!\n'
-                           'We''d like you to answer the following short questions before proceeding!\n\n'
+                           'We would like you to answer the following short questions before proceeding!\n\n'
                            '01. What are you looking for in a guild? i.e. Payouts, socializing, guidance in game\n\n'
                            '02. Have you been in a guild before? If YES, what guild and why did you leave?\n\n'
                            '03. What kind of player do you consider yourself as? PVE, PVP, Lifeskill-oriented, or well-balanced?\n\n'
                            '04. OPTIONAL - What is your nationality?\n\n'
                            'By answering these questions, you also acknowledge that Pastries is a chill international PVE/'
                            'Lifeskill guild, we do not tolerate any form of unhealthy or immature behaviours when you are with us!\n\n'
-                           'Thank you for taking your time to answer the questions - Please mention Eclair when you are done\n'
+                           'Thank you for answering the questions - Please mention Eclair when you are done\n'
+                           'Meanwhile, head over to #rules to read our guild rules.\n'
                            'We look forward to get to know you better!\n\n'.format(member))
 
     @commands.command(name='help')
@@ -42,7 +43,8 @@ class General(commands.Cog):
                       "%wb - shows the upcoming world boss\n\n" \
                       "%bosshunter - (un)register the Boss Hunter role. "
                       "Receive notifications 30 minutes before world boss!\n\n" \
-                      "%mp - shows profit with(out) VP from selling in MP i.e. %mp 43500000\n\n" \
+                      "%mp - shows profit with(out) VP from selling in MP i.e. %mp 43500000\n\n"
+                      "%hystria - sends a map of hystria!" \
                       "%bug <message>- reports a bug to Kagi\n\n```")
         await ctx.send(content)
 
